fb5e1ca3345a8ea35c655a406e5a4d2e8b09e579
Ramakrishna <ramakrishnamb@palo-it.com>
2023-12-07 16:10:45 +0530

    Mango db with repository for Registration functionality added

--- src/main/java/springbootfundamentals/tickets/registration/Registration.java
+++ src/main/java/springbootfundamentals/tickets/registration/Registration.java
@@ -1,8 +1,14 @@
 package springbootfundamentals.tickets.registration;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping. Document;
+
+@Document("registrations")
 public record Registration(
-        Integer id,
-        Integer productId,
+       @Id String id,
+        @NotNull(message = "Product id is required") Integer productId,
         String ticketCode,
-        String attendeeName) {
+        @NotBlank(message = "Attendee name is required") String attendeeName) {
 }
---
+++ src/main/java/springbootfundamentals/tickets/registration/RegistrationController.java
@@ -0,0 +1,61 @@
+package springbootfundamentals.tickets.registration;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/registrations")
+public class RegistrationController {
+
+    private final RegistrationRepository registrationRepository;
+
+    public RegistrationController(RegistrationRepository registrationRepository) {
+        this.registrationRepository = registrationRepository;
+    }
+
+    // In memory post
+//    @PostMapping
+//    public Registration create(@RequestBody @Valid Registration registration) {
+//        return registrationRepository.create(registration);
+//    }
+
+    // mango db post
+        @PostMapping
+    public Registration create(@RequestBody @Valid Registration registration) {
+            String ticketCode = UUID.randomUUID().toString();
+
+            return registrationRepository.save(new Registration(
+                    null, registration.productId(), ticketCode, registration.attendeeName()));
+    }
+
+    @GetMapping(path = "/{ticketCode}")
+    public Registration get(@PathVariable("ticketCode") String ticketCode) {
+        return registrationRepository.findByTicketCode(ticketCode)
+                .orElseThrow(() -> new NoSuchElementException("Registration with ticket code " + ticketCode + " not found"));
+    }
+
+    // In memory example
+//    @PutMapping
+//    public Registration update(@RequestBody Registration registration) {
+//        return registrationRepository.update(registration);
+//    }
+
+    @PutMapping
+    public Registration update(@RequestBody Registration registration) {
+        // Lookup the existing registration by ticket code
+        String ticketCode = registration.ticketCode();
+        var existing = registrationRepository.findByTicketCode(ticketCode)
+                .orElseThrow(() -> new NoSuchElementException("Registration with ticket code " + ticketCode + " not found"));
+
+        // Only update the attendee name
+        return registrationRepository.save(new Registration(
+                existing.id(), existing.productId(), ticketCode, registration.attendeeName()));
+    }
+    @DeleteMapping(path = "/{ticketCode}")
+    public void delete(@PathVariable("ticketCode") String ticketCode) {
+        registrationRepository.deleteByTicketCode(ticketCode);
+    }
+}
