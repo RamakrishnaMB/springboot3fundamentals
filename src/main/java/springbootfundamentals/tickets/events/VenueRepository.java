697379e937fd3218a6ee0936bf25efee668b3953
Ramakrishna <ramakrishnamb@palo-it.com>
2023-12-22 16:35:34 +0800

    External profile to connect postgres Sql running docker

--- src/main/java/springbootfundamentals/tickets/events/VenueRepository.java
+++ src/main/java/springbootfundamentals/tickets/events/VenueRepository.java
@@ -5,14 +5,18 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public class VenueRepository {
+//@Repository
+//public class VenueRepository {
+//
+//    private final List<Venue> venues = List.of(
+//            new Venue(201, "Globomatics Main Office", "Test Street 325", "New York", "USA"),
+//            new Venue(202, "Sea View Hotel", "Beach Boulevard 863", "Los Angeles", "USA"));
+//
+//    public Optional<Venue> findById(int id) {
+//        return venues.stream().filter(venue -> venue.id() == id).findAny();
+//    }
+//}
+import org.springframework.data.jpa.repository.JpaRepository;
 
-    private final List<Venue> venues = List.of(
-            new Venue(201, "Globomatics Main Office", "Test Street 325", "New York", "USA"),
-            new Venue(202, "Sea View Hotel", "Beach Boulevard 863", "Los Angeles", "USA"));
-
-    public Optional<Venue> findById(int id) {
-        return venues.stream().filter(venue -> venue.id() == id).findAny();
-    }
+public interface VenueRepository extends JpaRepository<Venue, Integer> {
 }
